b36c108005c69ca90395c33a7f264f0b2c180e3e
Brandon Trabon <brandon.trabon@gmail.com>
2021-06-08 17:35:38 +1000

    Found an issue while testing the Date/Time component where the scroll position wasn't being taken into account.

--- src/components/dom-handler.ts
+++ src/components/dom-handler.ts
@@ -10,22 +10,23 @@ export class DomHandler {
     const targetRectangle = target.getBoundingClientRect();
     let top = 0;
     let left = 0;
+    const scrollTop = window.pageYOffset;
 
     switch (position) {
       case AlignmentPosition.BottomLeft:
-        top = targetRectangle.top + targetRectangle.height;
+        top = targetRectangle.top + targetRectangle.height + scrollTop;
         left = targetRectangle.left;
         break;
       case AlignmentPosition.TopLeft:
-        top = targetRectangle.top;
+        top = targetRectangle.top + scrollTop;
         left = targetRectangle.left;
         break;
       case AlignmentPosition.BottomRight:
-        top = targetRectangle.top + targetRectangle.height;
+        top = targetRectangle.top + targetRectangle.height + scrollTop;
         left = targetRectangle.left + targetRectangle.width;
         break;
       case AlignmentPosition.TopRight:
-        top = targetRectangle.top;
+        top = targetRectangle.top + scrollTop;
         left = targetRectangle.left + targetRectangle.width;
     }
 
