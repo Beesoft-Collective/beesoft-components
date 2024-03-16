ce50b18381214fb8dc419a9cdd5376efc0674dda
Brandon Trabon <brandon.trabon@gmail.com>
2024-03-16 14:23:11 +1000

    BSC-31: Finished the advanced features to make the button more usable. (#118)

--- src/components/navigation/buttons/button/button.component.tsx
+++ src/components/navigation/buttons/button/button.component.tsx
@@ -1,5 +1,6 @@
 import cx from 'classnames';
 import { forwardRef, Ref, useImperativeHandle, useRef } from 'react';
+import { FocusRingStyle, useFocusRingStyle } from '../../../../common/hooks/style/use-focus-ring-style.ts';
 import { ButtonProps, ButtonRef } from './button.props.ts';
 
 const ButtonComponent = (props: ButtonProps, ref: Ref<ButtonRef>) => {
@@ -7,6 +8,7 @@ const ButtonComponent = (props: ButtonProps, ref: Ref<ButtonRef>) => {
 
   const {
     buttonType,
+    buttonStyle = 'flat',
     type = 'button',
     disabled = false,
     form,
@@ -28,17 +30,21 @@ const ButtonComponent = (props: ButtonProps, ref: Ref<ButtonRef>) => {
     click,
   }));
 
+  const focusRingStyles = useFocusRingStyle(FocusRingStyle.Focus);
   const buttonStyles = cx(
     'bc-button max-sm:bsc-p-4 md:bsc-p-2 dark:bsc-bg-mono-light-1 dark:hover:bsc-bg-mono-light-2 dark:bsc-text-mono-dark-1',
+    focusRingStyles,
     {
       'bsc-w-full': fullWidth,
-      'bc-disabled bsc-bg-gray-3 bsc-text-gray-2': disabled,
-      'bsc-bg-primary-1 bsc-text-white hover:bsc-bg-primary-2': buttonType === 'primary',
-      'bsc-bg-primary-2 bsc-text-white hover:bsc-bg-primary-3': buttonType === 'secondary',
-      'bsc-bg-info bsc-text-white': buttonType === 'info',
-      'bsc-bg-success bsc-text-white': buttonType === 'success',
-      'bsc-bg-warning bsc-text-white': buttonType === 'warning',
-      'bsc-bg-error bsc-text-white': buttonType === 'error',
+      'bc-disabled bsc-bg-gray-3 bsc-text-gray-2 bsc-pointer-events-none': disabled,
+      'bsc-bg-primary-1 bsc-text-white hover:bsc-bg-primary-2 focus:bsc-ring-primary-1': buttonType === 'primary',
+      'bsc-bg-primary-2 bsc-text-white hover:bsc-bg-primary-3 focus:bsc-ring-primary-2': buttonType === 'secondary',
+      'bsc-bg-info bsc-text-white focus:bsc-ring-info': buttonType === 'info',
+      'bsc-bg-success bsc-text-white focus:bsc-ring-success': buttonType === 'success',
+      'bsc-bg-warning bsc-text-white focus:bsc-ring-warning': buttonType === 'warning',
+      'bsc-bg-error bsc-text-white focus:bsc-ring-error': buttonType === 'error',
+      'bsc-rounded-md': buttonStyle === 'curved',
+      'bsc-rounded-full !bsc-px-4': buttonStyle === 'round',
     },
     className
   );
--- src/components/navigation/buttons/button/button.props.ts
+++ src/components/navigation/buttons/button/button.props.ts
@@ -1,9 +1,11 @@
 import { ButtonHTMLAttributes } from 'react';
 
 export type ButtonType = 'primary' | 'secondary' | 'info' | 'success' | 'warning' | 'error';
+export type ButtonStyle = 'flat' | 'curved' | 'round';
 
 export interface ButtonProps extends Omit<ButtonHTMLAttributes<HTMLButtonElement>, 'onClick'> {
   buttonType?: ButtonType;
+  buttonStyle?: ButtonStyle;
   fullWidth?: boolean;
   onClick?: () => void;
 }
