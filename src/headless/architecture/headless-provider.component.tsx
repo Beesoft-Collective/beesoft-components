7ad01e5e90added04d96175b122fbc276bb078e1
brandontrabon <brandon.trabon@gmail.com>
2024-05-01 17:29:02 +1000

    BSC-69: Moved the item scroller to the proper section of the headless directory. Created the first draft of the headless architecture for the team to review.

---
+++ src/headless/architecture/headless-provider.component.tsx
@@ -0,0 +1,17 @@
+import { PropsWithChildren } from 'react';
+import { HeadlessProviderProps } from './headless-provider.props.ts';
+import { HeadlessContext } from './use-headless-context.ts';
+
+/**
+ * This component contains the code to pass context information to child headless components. For our framework the data
+ * being passed are Signal objects that will be passed the actual data that is needed by the child component.
+ * @param {Record<string, Signal<any>>} props - A JavaScript object of the named Signal objects; this should be memo'd in the parent component.
+ * @param {React.ReactElement<any, string | React.JSXElementConstructor<any>> | string | number | Iterable<React.ReactNode> | React.ReactPortal | boolean | undefined | null} children - The JSX to wrap the context around.
+ * @returns {JSX.Element}
+ * @constructor
+ */
+const HeadlessProvider = ({ props, children }: PropsWithChildren<HeadlessProviderProps>) => {
+  return <HeadlessContext.Provider value={props}>{children}</HeadlessContext.Provider>;
+};
+
+export { HeadlessProvider };
