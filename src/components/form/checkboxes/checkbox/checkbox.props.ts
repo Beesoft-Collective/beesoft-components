91ef243ddf3984f131b778c62941233800313bbb
Brandon Trabon <brandon.trabon@gmail.com>
2024-06-03 23:22:50 +1000

    BSC-89: Re-created the `CheckboxChangeEvent` and `CheckboxRef` types, since they were already being used. (#133)

--- src/components/form/checkboxes/checkbox/checkbox.props.ts
+++ src/components/form/checkboxes/checkbox/checkbox.props.ts
@@ -1,7 +1,23 @@
+import { ReplacePropertyType } from '@beesoft/common';
+import { ChangeEvent } from 'react';
 import { ComponentAnimationProps } from '../../../../headless/components/component-interfaces.ts';
 import { HeadlessCheckboxProps } from '../../../../headless/components/form/checkboxes/checkbox/headless-checkbox.props.ts';
 import { SelectionLabelLocation } from '../../form-generic.interfaces.ts';
 
-export interface CheckboxProps extends HeadlessCheckboxProps, ComponentAnimationProps {
+export interface CheckboxChangeEvent {
+  name?: string;
+  value: unknown;
+  checked: boolean;
+  originalEvent?: ChangeEvent<HTMLInputElement>;
+}
+
+export interface CheckboxProps
+  extends ReplacePropertyType<HeadlessCheckboxProps, 'onChange', (value?: CheckboxChangeEvent) => void>,
+    ComponentAnimationProps {
   labelLocation?: SelectionLabelLocation;
+}
+
+export interface CheckboxRef {
+  setPartiallyChecked: (partiallyChecked: boolean) => void;
+  setChecked: (checked: boolean) => void;
 }
