9046ebcd833c86b7ee377654453c7e89e9e34fee
Brandon Trabon <brandon.trabon@gmail.com>
2024-10-06 00:25:03 +1000

    BSC-92 - Fix Focus Ring (#142)
    * BSC-92 - Fixed issue with the focus ring not changing when a theme colour is set.
    
    * BSC-92 - Added a CHANGELOG.md entry, so the component can be deployed.

--- src/headless/architecture/components/headless-field.component.tsx
+++ src/headless/architecture/components/headless-field.component.tsx
@@ -8,6 +8,8 @@ const HeadlessField = ({ id, name, value, type, checked, readOnly, className }: 
       value={value as string | number}
       type={type}
       checked={checked}
+      // this is needed to resolve errors when a component is used in a form library like hook form
+      onChange={() => {}}
       readOnly={readOnly}
       disabled={readOnly}
       className={className}
