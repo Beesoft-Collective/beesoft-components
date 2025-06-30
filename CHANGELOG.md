de967538cd611a47ca2fed95004875f03c673c4b
Brandon Trabon <brandon.trabon@gmail.com>
2025-06-30 13:42:26 +1000

    BSC-102 - Added the important flag to the border width and style properties. This is to fix an issue when using Tailwind 4 in a site still using Tailwind 3.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## Changed
+
+- Setting the important flag on a couple of classes; this should fix a styling issue when the library is implemented in a project still using Tailwind 3.
+
 ## [0.10.1-4] - 2025-06-28
 
 ## Changed
--- src/components/overlay/overlay-panel/overlay-panel.component.tsx
+++ src/components/overlay/overlay-panel/overlay-panel.component.tsx
@@ -294,7 +294,7 @@ const OverlayPanel = ({
     >
       {({ state, defaultStyle, transitionStyles }) => (
         <div
-          className="bsc:fixed bsc:border bsc:border-solid bsc:border-black bsc:bg-white bsc:shadow bsc:dark:border-mono-light-1 bsc:dark:bg-mono-dark-1 bsc:dark:text-mono-light-1"
+          className="bsc:fixed bsc:border! bsc:border-solid! bsc:border-black bsc:bg-white bsc:shadow bsc:dark:border-mono-light-1 bsc:dark:bg-mono-dark-1 bsc:dark:text-mono-light-1"
           style={{
             ...baseStyles,
             ...defaultStyle,
